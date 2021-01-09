ee069ac61831ac2f91e35dd31a3cca79f58d2d4c
Alberto Sonnino <asonnino@fb.com>
2021-01-09 22:33:40 +0000

    Rename node -> consensus

--- consensus/src/bin/node.rs
+++ consensus/src/bin/node.rs
@@ -1,9 +1,9 @@
 use clap::{crate_name, crate_version, App, AppSettings, SubCommand};
 use env_logger::Env;
 use futures::future::try_join_all;
-use node::config::Config as _;
-use node::config::{Committee, Secret};
-use node::node::Node;
+use consensus::config::Config as _;
+use consensus::config::{Committee, Secret};
+use consensus::node::Node;
 use log::error;
 use std::fs;
 use tokio::task::JoinHandle;
