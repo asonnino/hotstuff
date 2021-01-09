e0b09ada3dc1021eb9f25b44b102d0cda20fa442
Alberto Sonnino <asonnino@fb.com>
2021-01-09 17:30:59 +0000

    Mempool driver

--- node/src/mempool.rs
+++ node/src/mempool.rs
@@ -1,6 +1,17 @@
+use crate::core::{CoreMessage, RoundNumber};
+use crate::error::ConsensusResult;
+use crate::messages::Block;
+use crate::store::Store;
 use async_trait::async_trait;
+use futures::future::FutureExt as _;
+use futures::select;
+use futures::stream::futures_unordered::FuturesUnordered;
+use futures::stream::StreamExt as _;
+use log::{debug, error};
 use rand::Rng as _;
+use std::collections::HashMap;
 use std::marker::Send;
+use tokio::sync::mpsc::{channel, Receiver, Sender};
 
 #[async_trait]
 pub trait NodeMempool: Send + Sync {
@@ -18,6 +29,94 @@ pub trait NodeMempool: Send + Sync {
     /// knowledge that a block is committed to clean up its internal state.
     async fn cleanup(&self, payload: &[u8]);
 }
+
+type DriverMessage = (Vec<u8>, Block, Receiver<()>);
+
+pub struct MempoolDriver<Mempool> {
+    inner_channel: Sender<DriverMessage>,
+    mempool: Mempool,
+    pending: HashMap<RoundNumber, Sender<()>>,
+}
+
+impl<Mempool: 'static + NodeMempool> MempoolDriver<Mempool> {
+    pub fn new(mempool: Mempool, core_channel: Sender<CoreMessage>, store: Store) -> Self {
+        let (tx_inner, mut rx_inner): (_, Receiver<DriverMessage>) = channel(1000);
+        let mut waiting = FuturesUnordered::new();
+        tokio::spawn(async move {
+            loop {
+                select! {
+                    message = rx_inner.recv().fuse() => {
+                        if let Some((wait_on, block, handler)) = message {
+                            let fut = Self::waiter(store.clone(), wait_on, block, handler);
+                            waiting.push(fut);
+                        }
+                    },
+                    result = waiting.select_next_some() => {
+                        match result {
+                            Ok(Some(block)) => {
+                                let message = CoreMessage::LoopBack(block);
+                                if let Err(e) = core_channel.send(message).await {
+                                    panic!("Failed to send message through core channel: {}", e);
+                                }
+                            },
+                            Ok(None) => (),
+                            Err(e) => error!("{}", e)
+                        }
+                    }
+                }
+            }
+        });
+        Self {
+            inner_channel: tx_inner,
+            mempool,
+            pending: HashMap::new(),
+        }
+    }
+
+    async fn waiter(
+        mut store: Store,
+        wait_on: Vec<u8>,
+        deliver: Block,
+        mut handler: Receiver<()>,
+    ) -> ConsensusResult<Option<Block>> {
+        select! {
+            result = store.notify_read(wait_on).fuse() => {
+                let _ = result?;
+                Ok(Some(deliver))
+            },
+            _ = handler.recv().fuse() => {
+                Ok(None)
+            }
+        }
+    }
+
+    pub async fn verify(&mut self, wait_on: Vec<u8>, block: &Block) {
+        // call Verify
+
+        if !self.pending.contains_key(&block.round) {
+            let (tx_cancel, rx_cancel) = channel(1);
+            let round = block.round;
+            self.pending.insert(round, tx_cancel);
+            debug!("Registering missing payload for {:?}", block);
+            let message = (block.payload.clone(), block.clone(), rx_cancel);
+            if let Err(e) = self.inner_channel.send(message).await {
+                panic!("Failed to send request to synchronizer: {}", e);
+            }
+        }
+    }
+
+    pub async fn timeout(&mut self, round: RoundNumber) {
+        // TODO: write it better
+        for (k, v) in &self.pending {
+            if !v.is_closed() && k < &round {
+                let _ = v.send(()).await;
+            }
+        }
+        self.pending.retain(|k, _| k < &round);
+    }
+}
+
+// ---
 
 pub struct MockMempool;
 
