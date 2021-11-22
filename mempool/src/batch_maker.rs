0992881a53158463a1ade1a865b6ed4238cb102b
Alberto Sonnino <asonnino@fb.com>
2021-11-22 14:42:25 +0000

    Improve control system

--- mempool/src/batch_maker.rs
+++ mempool/src/batch_maker.rs
@@ -26,7 +26,7 @@ pub type Transaction = Vec<u8>;
 pub type Batch = Vec<Transaction>;
 
 /// The maximum number of batches that have been created but are not yet certified.
-const MAX_PENDING_BATCHES: usize = 10;
+const MAX_PENDING_BATCHES: usize = 5;
 
 /// Assemble clients transactions into batches.
 pub struct BatchMaker {
