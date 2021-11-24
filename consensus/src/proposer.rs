ff695874414323ca8abb9e5118f83695af37f018
Alberto Sonnino <asonnino@fb.com>
2021-11-24 15:32:42 +0000

    Test teak control system

--- consensus/src/proposer.rs
+++ consensus/src/proposer.rs
@@ -174,7 +174,7 @@ impl Proposer {
                     ProposerMessage::Cleanup(digests) => {
                         for x in &digests {
                             if self.buffer.remove(x) {
-                                others_payloads -= 1;
+                                others_payloads -= 1; // TODO: May panic!
                             }
                         }
                     }
