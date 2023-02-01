1557106a99c55b03429ed7fdde31df7a94a699a2
Alberto Sonnino <alberto@sonnino.com>
2023-02-01 20:37:30 +0900

    BLS - EdDSA comparison

--- benchmark/benchmark/plot.py
+++ benchmark/benchmark/plot.py
@@ -1,5 +1,6 @@
 from re import findall, search, split
 import matplotlib.pyplot as plt
+import matplotlib.ticker as tick
 from matplotlib.ticker import StrMethodFormatter
 from glob import glob
 from itertools import cycle
@@ -7,6 +8,24 @@ from itertools import cycle
 from benchmark.utils import PathMaker
 from benchmark.config import PlotParameters
 from benchmark.aggregate import LogAggregator
+
+
+@tick.FuncFormatter
+def sec_major_formatter(x, pos):
+    if pos is None:
+        return
+    return f'{float(x)/10_00:.0f}'
+    # return f'{x:,.0f}'
+
+
+@tick.FuncFormatter
+def k_major_formatter(x, pos):
+    if pos is None:
+        return
+    if float(x) > 1_000:
+        return f'{float(x)/1_000:,.0f}k'
+    else:
+        return f'{x:,.0f}'
 
 
 class PlotError(Exception):
@@ -53,8 +72,8 @@ class Ploter:
         size = int(search(r'Transaction size: (\d+)', data).group(1))
         return x * 10**6 / size
 
-    def _plot(self, x_label, y_label, y_axis, z_axis, type):
-        plt.figure()
+    def _plot(self, x_label, y_label, y_axis, z_axis, type, y_max=None):
+        plt.figure(figsize=(6.4, 4.2))
         markers = cycle(['o', 'v', 's', 'p', 'D', 'P'])
         self.results.sort(key=self._natural_keys, reverse=(type == 'tps'))
         for result in self.results:
@@ -65,18 +84,26 @@ class Ploter:
 
             plt.errorbar(
                 x_values, y_values, yerr=y_err, label=z_axis(result),
-                linestyle='dotted', marker=next(markers), capsize=3
+                linestyle='dotted', marker=next(markers), capsize=3,
+                markersize=10, elinewidth=0.5
             )
 
-        plt.legend(loc='lower center', bbox_to_anchor=(0.5, 1), ncol=2)
+        plt.legend(
+            loc='lower center', bbox_to_anchor=(0.5, 1), ncol=3, prop={'size': 13}
+        )
         plt.xlim(xmin=0)
-        plt.ylim(bottom=0)
-        plt.xlabel(x_label)
-        plt.ylabel(y_label[0])
+        plt.ylim(bottom=0, top=y_max)
+        plt.xlabel(x_label, fontweight='bold', fontsize=16)
+        plt.ylabel(y_label[0], fontweight='bold', fontsize=16)
+        plt.xticks(weight='bold', fontsize=16)
+        plt.yticks(weight='bold', fontsize=16)
         plt.grid()
         ax = plt.gca()
-        ax.xaxis.set_major_formatter(StrMethodFormatter('{x:,.0f}'))
+        ax.xaxis.set_major_formatter(k_major_formatter)
         ax.yaxis.set_major_formatter(StrMethodFormatter('{x:,.0f}'))
+        ax.xaxis.set_major_locator(tick.MultipleLocator(base=20_000))
+        if 'latency' in type:
+            ax.yaxis.set_major_formatter(sec_major_formatter)
         if len(y_label) > 1:
             secaxy = ax.secondary_yaxis(
                 'right', functions=(self._tps2bps, self._bps2tps)
@@ -112,14 +139,16 @@ class Ploter:
         ploter._plot(x_label, y_label, ploter._tps, z_axis, 'robustness')
 
     @classmethod
-    def plot_latency(cls, files):
+    def plot_latency(cls, files, y_max):
         assert isinstance(files, list)
         assert all(isinstance(x, str) for x in files)
         z_axis = cls.nodes
         x_label = 'Throughput (tx/s)'
-        y_label = ['Latency (ms)']
+        y_label = ['Latency (s)']
         ploter = cls(files)
-        ploter._plot(x_label, y_label, ploter._latency, z_axis, 'latency')
+        ploter._plot(
+            x_label, y_label, ploter._latency, z_axis, 'latency', y_max=y_max
+        )
 
     @classmethod
     def plot_tps(cls, files):
@@ -144,7 +173,7 @@ class Ploter:
         # Load the aggregated log files.
         robustness_files, latency_files, tps_files = [], [], []
         tx_size = params.tx_size
-        
+
         for f in params.faults:
             for n in params.nodes:
                 robustness_files += glob(
@@ -159,6 +188,7 @@ class Ploter:
                 )
 
         # Make the plots.
+        y_max = 10_000
         cls.plot_robustness(robustness_files)
-        cls.plot_latency(latency_files)
+        cls.plot_latency(latency_files, y_max)
         cls.plot_tps(tps_files)
